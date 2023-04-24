863b419f83fdd1f9c252621dce9bb94666c9b7cb
Line Ibo <ibo.line@hotmail.fr>
2023-04-24 10:55:36 +0200

    premier commit

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,31 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector (".arrow_left");
+const arrowRight = document.querySelector (".arrow_right");
+const dots = document.querySelector (".dots");
+const dot = document.querySelector (".dot")
+const dotSelected = document.querySelector ("dot_selected");
+const bannerImg = document.querySelector (".banner-img")
+
+arrowRight.addEventListener("click", function() {
+	
+})
+arrowLeft.addEventListener("click", function() {
+})
