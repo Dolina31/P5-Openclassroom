5de290abf304ce34a9d2dbb739354f1c3a386af7
Line Ibo <ibo.line@hotmail.fr>
2023-04-24 21:41:27 +0200

    ajout du changement d'image au click des flèches

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,47 @@
 const slides = [
 	{
-		"image":"./assets/images/slideshow/slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "./assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "./assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "./assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"./assets/images/slideshow/slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "./assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-const arrowLeft = document.querySelector (".arrow_left");
-const arrowRight = document.querySelector (".arrow_right");
-const dots = document.querySelector (".dots");
-const dot = document.querySelector (".dot")
-const dotSelected = document.querySelector ("dot_selected");
-const bannerImg = document.querySelector (".banner-img")
+const ArrowRight = document.querySelector(".arrow_right");
+const ArrowLeft = document.querySelector(".arrow_left");
+const dots = document.querySelector(".dots");
+const dot = document.querySelector(".dot");
+const dotSelected = document.querySelector(".dot_selected");
 
-arrowRight.addEventListener("click", function() {
-	
+let bannerImg = document.querySelector(".banner-img");
+bannerImgIndex = 0
+dot.classList.add("dot-selected");
+
+ArrowRight.addEventListener("click", () => {
+	if (bannerImgIndex < slides.length - 1) {
+		bannerImgIndex++
+		bannerImg.src = slides[bannerImgIndex].image
+	}
 })
-arrowLeft.addEventListener("click", function() {
+ArrowLeft.addEventListener("click", () => {
+	if (bannerImgIndex < slides.length - 1) {
+		bannerImgIndex--
+		bannerImg.src = slides[bannerImgIndex].image
+	}
 })
+
+for (let i = 0; i < slides.length; i++) {
+	const dot = document.createElement("div");
+	dots.appendChild(dot);
+	dot.className = ("dot");
+}
