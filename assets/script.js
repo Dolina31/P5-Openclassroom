ab4fce3c8bf2a73cea79df22a9618a0b119cd25e
Line Ibo <ibo.line@hotmail.fr>
2023-04-28 14:07:49 +0200

    le caroussel boucle à l'infinie, le changement de l'image fonctionne, celui des tagLines aussi, les dots changent à chaque click. Le projet est terminé

--- assets/script.js
+++ assets/script.js
@@ -22,9 +22,6 @@ const ArrowLeft = document.querySelector(".arrow_left");
 
 const dots = document.querySelector(".dots");
 const dot = document.querySelectorAll(".dot");
-console.log(dot.length)
-
-
 
 let bannerImg = document.querySelector(".banner-img");
 bannerImgIndex = 0;
@@ -32,32 +29,41 @@ bannerImgIndex = 0;
 let tagLineSlide = document.querySelector("p");
 let tagLineSlideIndex = 0
 
-for (let i = 0; i < slides.length; i++) {
-	const dot = document.createElement("div");
-	dots.appendChild(dot);
-	dot.className = ("dot");
-}
-
 const dotSelected = document.querySelector(".dot_selected");
 let dotSelectedIndex = 0
 
+function updateCaroussel() {
+
+	dots.innerHTML = ''
+
+	for (let i = 0; i < slides.length; i++) {
+		const dot = document.createElement("div");
+		dots.appendChild(dot);
+		dot.className = ("dot");
+
+		if (i == dotSelectedIndex) {
+			dots.children[i].classList.add("dot_selected")
+		}
+	}
+	bannerImg.src = slides[bannerImgIndex].image;
+	tagLineSlide.innerHTML = `<p> ${slides[tagLineSlideIndex].tagLine} </p>`
+}
+updateCaroussel()
+
+
 
 ArrowRight.addEventListener("click", () => {
-	bannerImgIndex < slides.length - 1 ? bannerImgIndex++ : bannerImgIndex = 0;
-	bannerImg.src = slides[bannerImgIndex].image;
-
-	tagLineSlideIndex < slides.length - 1 ? tagLineSlideIndex++ : tagLineSlideIndex = 0;
-	tagLineSlide.innerHTML = `<p> ${slides[tagLineSlideIndex].tagLine} </p>`
+	bannerImgIndex = bannerImgIndex < slides.length - 1 ? bannerImgIndex + 1 : 0;
+	tagLineSlideIndex = tagLineSlideIndex < slides.length - 1 ? tagLineSlideIndex + 1 : 0;
+	dotSelectedIndex = dotSelectedIndex < slides.length - 1 ? dotSelectedIndex + 1 : 0;
+	updateCaroussel();
 });
 
 ArrowLeft.addEventListener("click", () => {
-	bannerImgIndex < slides.length ? bannerImgIndex-- : bannerImgIndex = 3;
-	bannerImg.src = slides[bannerImgIndex].image;
-	console.log(bannerImgIndex)
-
-	tagLineSlideIndex < slides.length ? tagLineSlideIndex-- : null;
-	tagLineSlide.innerHTML = `<p> ${slides[tagLineSlideIndex].tagLine} </p>`
+	bannerImgIndex = bannerImgIndex == 0 ? slides.length - 1 : bannerImgIndex - 1;
+	tagLineSlideIndex = tagLineSlideIndex == 0 ? slides.length - 1 : tagLineSlideIndex - 1;
+	dotSelectedIndex = dotSelectedIndex == 0 ? slides.length - 1 : dotSelectedIndex - 1;
+	updateCaroussel();
 });
-
 
 
