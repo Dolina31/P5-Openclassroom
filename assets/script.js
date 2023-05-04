af48a2c10df0127c8317298209b5610fe2daae88
Line Ibo <ibo.line@hotmail.fr>
2023-05-04 15:10:39 +0200

    retravail du js pour qu'il soit plus DRY

--- assets/script.js
+++ assets/script.js
@@ -15,59 +15,50 @@ const slides = [
 		"image": "./assets/images/slideshow/slide4.png",
 		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
 
 const ArrowRight = document.querySelector(".arrow_right");
 const ArrowLeft = document.querySelector(".arrow_left");
 
+const bannerImg = document.querySelector(".banner-img");
+
+const tagLineSlide = document.querySelector("p");
+
 const dots = document.querySelector(".dots");
-const dot = document.querySelectorAll(".dot");
 
-let bannerImg = document.querySelector(".banner-img");
-bannerImgIndex = 0;
-
-let tagLineSlide = document.querySelector("p");
-let tagLineSlideIndex = 0
-
-const dotSelected = document.querySelector(".dot_selected");
-let dotSelectedIndex = 0
+let slideIndex = 0;
 
 function updateCaroussel() {
 
-	dots.innerHTML = ''
+	dots.innerHTML = '';
 
 	for (let i = 0; i < slides.length; i++) {
 		const dot = document.createElement("div");
 		dots.appendChild(dot);
 		dot.className = ("dot");
 
-		if (i == dotSelectedIndex) {
-			dots.children[i].classList.add("dot_selected")
+		if (i == slideIndex) {
+			dots.children[i].classList.add("dot_selected");
 		}
 
 		dot.addEventListener("click", () => {
-			bannerImgIndex = i;
-			tagLineSlideIndex = i;
-			dotSelectedIndex = i;
+			slideIndex = i;
 			updateCaroussel();
-		})
+		});
 
-		bannerImg.src = slides[bannerImgIndex].image;
-		tagLineSlide.innerHTML = `<p> ${slides[tagLineSlideIndex].tagLine} </p>`;
+		bannerImg.src = slides[slideIndex].image;
+		tagLineSlide.innerHTML = `<p>${slides[slideIndex].tagLine}</p>`;
 	}
 }
-updateCaroussel()
+updateCaroussel();
+
 
 ArrowRight.addEventListener("click", () => {
-	bannerImgIndex = bannerImgIndex < slides.length - 1 ? bannerImgIndex + 1 : 0;
-	tagLineSlideIndex = tagLineSlideIndex < slides.length - 1 ? tagLineSlideIndex + 1 : 0;
-	dotSelectedIndex = dotSelectedIndex < slides.length - 1 ? dotSelectedIndex + 1 : 0;
+	slideIndex = slideIndex < slides.length - 1 ? slideIndex + 1 : 0;
 	updateCaroussel();
 });
 
 ArrowLeft.addEventListener("click", () => {
-	bannerImgIndex = bannerImgIndex == 0 ? slides.length - 1 : bannerImgIndex - 1;
-	tagLineSlideIndex = tagLineSlideIndex == 0 ? slides.length - 1 : tagLineSlideIndex - 1;
-	dotSelectedIndex = dotSelectedIndex == 0 ? slides.length - 1 : dotSelectedIndex - 1;
+	slideIndex = slideIndex == 0 ? slides.length - 1 : slideIndex - 1;
 	updateCaroussel();
 });
